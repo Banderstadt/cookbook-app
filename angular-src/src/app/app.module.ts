c884c432b547196ba7eac9894a3eae6d7b0c9e4b
Vasyl Zadorozhnyy <amdpaxton@gmail.com>
2018-03-23 21:25:15 +0200

    added ng-bootstrap

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { CookbookComponent } from './components/cookbook/cookbook.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CookbookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
